836f983b7e775d9347cc6c0875c3b4a1c3f0c921
dogghoodie <dogghoodie@gmail.com>
2024-09-17 03:29:58 -0500

    Changed "Background" to "Game Hud" and updated the canvas colors.

--- top-down-tank-game/src/App.js
+++ top-down-tank-game/src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Background</h1>
+      <h1 style={{ color: '#6272a4' }}>Game Hud</h1>
+      <canvas
+        id="gameBackground"
+        width="1600"
+        height="1000"
+        style={{ backgroundColor: 'black' }}>
+      </canvas>
     </div>
   );
 }
