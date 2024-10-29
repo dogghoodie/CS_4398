a309d95c5dc8f8f05ea47e9a0ddc348a874eaafd
Your Name <michaelalex03@outlook.com>
2024-10-28 21:48:42 -0500

    added post for adding user and scores

---
+++ server/models/scores.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const scoreSchema = new mongoose.Schema({
+    playerName: {
+        type: String,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    }
+});
+
+// Create the Score collection(pluralizes the names in the collection name ex. creates collection as name scores instead of score)
+module.exports = mongoose.model('Score', scoreSchema);
+
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const cors = require('cors');
-const User = require('./models/users.js');
+const Score = require('./models/scores');
 
 mongoose.set('strictQuery', false); //allows for quering of fields not defined in the schema
 
@@ -14,44 +14,58 @@ const port = 3001;
 app.use(cors());
 app.use(express.json())
 
-const connectDB = async () => {
+
+/*Start method that connects Nodejs backend to the mongodb Database*/
+const start = async () => {
     try {
         console.log('MongoDB URI:', process.env.MONGO_URI);
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
+
+        app.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`);
+        });
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
         process.exit(1); // Exit the process with failure
     }
 };
 
+start();
 
-app.get("/", (req, res) => {
-    res.send("Welcome");
-})
 
-//Returns all users in database
-app.get("/api/getUsers" , async (req, res) => {
-    console.log(await mongoose.connection.db.listCollections().toArray()); //gets a list of all the collections within the database
+
+
+
+
+app.get("/test", (req, res) => {
+    res.json(score);
+});
+
+//Returns all scores in database
+app.get("/api/getScores" , async (req, res) => {
+   
     try{
-        const result = await User.find();
-        res.json({"users": result});
+        const result = await Score.find(); //Gets all data with no filter from scores collection
+        console.log(result);
+        res.json({"scores": result});
     }catch(e){
-        res.status(500).json({error: "e.message"});
+        res.status(500).json({error: e.message});
+    }
+});
+
+//Adds user and the scores to database
+app.post("/addScores", async (req, res) => {
+    console.log(req.body)
+    try{
+        const score = new Score(req.body);
+        await score.save() //await needed so we can see if there is a name and score given if not throw error
+        res.status(201).json({score}) //shorthand for score: score
+    }catch(e){
+        res.status(500).json({error: e.message});
     }
 });
 
 
-const start = async () => {
-    await connectDB(); // Connect to MongoDB
-    app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`);
-    });
-};
-
-start();
 
 
